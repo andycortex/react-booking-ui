057303f1cce1a6b59f0d6d5e0c5f10d58a829057
Cortex <cortex.andy2809@gmail.com>
2022-05-06 23:14:56 -0400

    design header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { 
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
 import Home from "./pages/home/Home";
 
 function App() {
   return (
-    <Home/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
